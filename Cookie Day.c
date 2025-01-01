34770c1079f0e475b8d03003198d4358d1091632
Ankur Tiwari <tiwariankur2004@gmail.com>
2025-01-01 12:11:49 +0530

    Cookie Day.c
    Solution to Codechef problem "Cookie Day" in C language

--- Cookie Day.c
+++ Cookie Day.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+int main() {
+	int t,n,k,q,r,w=0,i,c;
+	scanf("%d",&t);
+	while(t>0){
+	    scanf("%d",&n);
+	    scanf("%d",&k);
+	     w=k;
+	    
+	    int a[n];
+	    c=0;
+	    for(i=0;i<n;i++){
+	        scanf("%d",&a[i]);
+	        q=a[i]/k;
+	        
+	        if(q==0){
+	            c++;
+	            continue;
+	        }
+	       
+	        r=a[i]%k;
+	        if(r<w){
+	            w=r;
+	        }
+	        
+	    }
+	    if(q==0&& c==n){
+	        printf("%d\n",-1);
+	    }
+	    else{
+	        printf("%d\n",w);
+	    }
+	    t--;
+	    
+	}
+
+}
+
 
